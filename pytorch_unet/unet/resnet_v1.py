2d343bbb18f0a977a8cc67e36458d31c5a431603
Allen Qin <allen.qin.au@gmail.com>
2018-09-13 16:25:19 +1000

    modified unet using resnet backbone

--- pytorch_unet/unet/resnet_v1.py
+++ pytorch_unet/unet/resnet_v1.py
@@ -7,101 +7,18 @@ import torchvision
 import torch.utils.model_zoo as model_zoo
 
 
-class double_conv(nn.Module):
-    '''(conv => BN => ReLU) * 2'''
-    def __init__(self, in_ch, out_ch):
-        super(double_conv, self).__init__()
-        self.conv = nn.Sequential(
-            nn.Conv2d(in_ch, out_ch, 3, padding=1),
-            nn.BatchNorm2d(out_ch),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(out_ch, out_ch, 3, padding=1),
-            nn.BatchNorm2d(out_ch),
-            nn.ReLU(inplace=True)
-        )
+class conv_2d_depth_sep(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1, 
+                 padding=0, dilation=1):
+        super(conv_2d_depth_sep, self).__init__()
+        self.depthwise = nn.Conv2d(in_channels, in_channels, kernel_size=kernel_size, 
+                                   padding=padding, groups=in_channels, dilation=dilation)
+        self.pointwise = nn.Conv2d(in_channels, out_channels, kernel_size=1)
 
     def forward(self, x):
-        x = self.conv(x)
-        return x
-
-
-class inconv(nn.Module):
-    def __init__(self, in_ch, out_ch):
-        super(inconv, self).__init__()
-        self.conv = double_conv(in_ch, out_ch)
-
-    def forward(self, x):
-        x = self.conv(x)
-        return x
-
-
-class down(nn.Module):
-    def __init__(self, in_ch, out_ch):
-        super(down, self).__init__()
-        self.mpconv = nn.Sequential(
-            nn.MaxPool2d(2),
-            double_conv(in_ch, out_ch)
-        )
-
-    def forward(self, x):
-        #print(f'in: {x.shape}')
-        x = self.mpconv(x)
-        #print(f'out: {x.shape}')
-        return x
-
-
-class up(nn.Module):
-    def __init__(self, in_ch, out_ch, trans_in_ch, bilinear=False):
-        super(up, self).__init__()
-
-        #  would be a nice idea if the upsampling could be learned too,
-        #  but my machine do not have enough memory to handle all those weights
-												 
-        if bilinear:
-            print('Using bilinear for upsampling')
-            self.upscale = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
-        else:
-            print('Using transpose conv for upsampling')
-            self.upscale = nn.ConvTranspose2d(trans_in_ch, trans_in_ch, 2, stride=2)
-																					
-        self.conv = double_conv(in_ch, out_ch)
-
-    def forward(self, x1, x2):
-        #print(f'in: {x1.shape}, {x2.shape}')
-        x1 = self.upscale(x1)
-        diffX = x1.size()[2] - x2.size()[2]
-        diffY = x1.size()[3] - x2.size()[3]
-        x2 = F.pad(x2, (diffX // 2, int(diffX / 2),
-                        diffY // 2, int(diffY / 2)))
-        x = torch.cat([x2, x1], dim=1)
-        x = self.conv(x)
-        #print(f'out: {x.shape}')
-        return x
-
-
-class outconv(nn.Module):
-    def __init__(self, in_ch, out_ch, logits=False):
-        super(outconv, self).__init__()
-        self.conv = nn.Conv2d(in_ch, out_ch, 1)
-        self.conv_mask = nn.Conv2d(in_ch, out_ch, 128)
-        self.sig = nn.Sigmoid()
-        self.logits = logits
-            
-
-    def forward(self, x):
-        x_conv = self.conv(x)        
-        if not self.logits:
-            x_out = self.sig(x_conv)
-        else:
-            x_out = x_conv
-            
-        crop_start = (x.shape[-1]-101)//2
-        crop_end = crop_start + 101
-        x_out = x_out[:,:,crop_start:crop_end,crop_start:crop_end].squeeze()
-        
-        return x_out
-
-
+        out = self.depthwise(x)
+        out = self.pointwise(out)
+        return out
 
 
 def conv3x3(in_planes, out_planes, stride=1):
@@ -187,7 +104,8 @@ def resnet18unet(in_ch=1, bilinear=True, pretrained=False, **kwargs):
     Args:
         
     """
-    model = UResNet(BasicBlock, [2, 2, 2, 2], in_ch=in_ch, bilinear=bilinear, **kwargs)
+    model = UResNet(BasicBlock, [2, 2, 2, 2], up_ch=[768, 384, 192, 128],
+                    in_ch=in_ch, bilinear=bilinear, **kwargs)
     if pretrained:
         print('Using pre-trained model')
         trained_model = torchvision.models.resnet18(pretrained=True)
@@ -206,7 +124,8 @@ def resnet34unet(in_ch=1, bilinear=True, pretrained=False, **kwargs):
     Args:
         
     """
-    model = UResNet(BasicBlock, [3, 4, 6, 3], in_ch=in_ch, bilinear=bilinear, **kwargs)
+    model = UResNet(BasicBlock, [3, 4, 6, 3], up_ch=[512, 384, 192, 128],
+                    in_ch=in_ch, bilinear=bilinear, **kwargs)
     if pretrained:
         print('Using pre-trained model')
         trained_model = torchvision.models.resnet34(pretrained=True)
@@ -225,7 +144,8 @@ def resnet50unet(in_ch=1, bilinear=True, pretrained=False, **kwargs):
     Args:
         
     """    
-    model = UResNet(Bottleneck, [3, 4, 6, 3], in_ch=in_ch, bilinear=bilinear, **kwargs)
+    model = UResNet(Bottleneck, [3, 4, 6, 3], up_ch=[3072, 1536, 768, 320],
+                    in_ch=in_ch, bilinear=bilinear, **kwargs)
     if pretrained:
         print('Using pre-trained model')
         trained_model = torchvision.models.resnet50(pretrained=True)
@@ -243,7 +163,8 @@ def resnet101unet(in_ch=1, bilinear=True, pretrained=False, **kwargs):
     Args:
         
     """    
-    model = UResNet(Bottleneck, [3, 4, 23, 3], in_ch=in_ch, bilinear=bilinear, **kwargs)
+    model = UResNet(Bottleneck, [3, 4, 23, 3], up_ch=[3072, 1536, 768, 320], 
+                    in_ch=in_ch, bilinear=bilinear, **kwargs)
     if pretrained:
         print('Using pre-trained model')
         trained_model = torchvision.models.resnet101(pretrained=True)
@@ -261,7 +182,8 @@ def resnet152unet(in_ch=1, bilinear=True, pretrained=False, **kwargs):
     Args:
         
     """    
-    model = UResNet(Bottleneck, [3, 8, 36, 3], in_ch=in_ch, bilinear=bilinear, **kwargs)
+    model = UResNet(Bottleneck, [3, 8, 36, 3], up_ch=[3072, 1536, 768, 320],
+                    in_ch=in_ch, bilinear=bilinear, **kwargs)
     if pretrained:
         print('Using pre-trained model')
         trained_model = torchvision.models.resnet152(pretrained=True)
@@ -276,7 +198,7 @@ def resnet152unet(in_ch=1, bilinear=True, pretrained=False, **kwargs):
     return model
 
 class UResNet(nn.Module):
-    def __init__(self, block, layers, in_ch=1, num_classes=1, bilinear=True):
+    def __init__(self, block, layers, up_ch, in_ch=1, num_classes=1, bilinear=True):
         print('Local ResNet')
         self.inplanes = 64
         super(UResNet, self).__init__()
@@ -285,18 +207,28 @@ class UResNet(nn.Module):
         self.bn1 = nn.BatchNorm2d(64)
         self.relu = nn.ReLU(inplace=True)
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-        self.layer1 = self._make_layer(block, 128, layers[0])
-        self.layer2 = self._make_layer(block, 256, layers[1], stride=2)
-        self.layer3 = self._make_layer(block, 512, layers[2], stride=2)
-        self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
-        #self.avgpool = nn.AvgPool2d(7, stride=1)
-        #self.fc = nn.Linear(512 * block.expansion, num_classes)
-        self.outc = outconv(64, num_classes)
-
-        self.up1 = up(1024*block.expansion, 256*block.expansion, 512*block.expansion, bilinear=bilinear)
-        self.up2 = up(512*block.expansion, 128*block.expansion, 256*block.expansion, bilinear=bilinear)
-        self.up3 = up(256*block.expansion, 64, 128, bilinear=bilinear)
-        self.up4 = up(128, 64, 64, bilinear=bilinear)
+        self.layer1 = self._make_layer(block, 64, layers[0])
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
+        self.layer4 = self._make_atrous_layer(block, 512, layers[3], stride=1, dilation=2)
+        
+        
+        self.pool1 = nn.Conv2d(512, 256, 1)
+        self.pool2 = Polling(in_ch=512, out_ch=256, upscale_size=8+2*2, dilation=2)
+        self.pool3 = Polling(in_ch=512, out_ch=256, upscale_size=8+2*4, dilation=4)
+        self.pool4 = Polling(in_ch=512, out_ch=256, upscale_size=8+2*6, dilation=6)        
+        self.conv_pool = nn.Conv2d(1024, 256, 1)        
+        self.conv_dec_low_level = nn.Conv2d(64, 256, 1)
+        
+        self.merg_conv = nn.Sequential(
+                conv3x3(512, 256, 1),
+                nn.BatchNorm2d(256),
+                nn.ReLU(inplace=True),
+                conv3x3(256, 1, 1),
+                #nn.BatchNorm2d(1),
+                #nn.ReLU(inplace=True)
+            )
+                                   
         
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
@@ -321,28 +253,96 @@ class UResNet(nn.Module):
             layers.append(block(self.inplanes, planes))
 
         return nn.Sequential(*layers)
+    
+    
+    def _make_atrous_layer(self, block, planes, blocks, stride=1, dilation=1):
+        downsample = None
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                conv_2d_depth_sep(self.inplanes, planes * block.expansion,
+                                  kernel_size=3, padding=dilation, stride=stride,
+                                  dilation=dilation),
+                nn.BatchNorm2d(planes * block.expansion),
+            )           
+                
 
-    def forward(self, x):
-        x = self.conv1(x)
+        layers = []
+        #print(downsample)
+        layers.append(block(self.inplanes, planes, stride, downsample))
+        self.inplanes = planes * block.expansion
+        for i in range(1, blocks):
+            layers.append(block(self.inplanes, planes))
+
+        return nn.Sequential(*layers)
+    
+    
+    def forward(self, x):        
+        print(f'x in: {x.shape}')
+        #x in: 128*128
+        x = self.conv1(x) #64*64
         x = self.bn1(x)
+        print(f'x conv1 out: {x.shape}') 
         #x1 = self.relu(x)
-        x1 = self.maxpool(self.relu(x))
-        x2 = self.layer1(x1)
-        x3 = self.layer2(x2)
-        x4 = self.layer3(x3)
-        x5 = self.layer4(x4)
+        x1 = self.maxpool(self.relu(x)) #32*32
+        print(f'x maxpool out: {x1.shape}')
+        x2 = self.layer1(x1) #32*32
+        print(f'x2: {x2.shape}')
+        x3 = self.layer2(x2) #16*16
+        print(f'x3: {x3.shape}')
+        x4 = self.layer3(x3) #8*8
+        print(f'x4: {x4.shape}')
+        x5 = self.layer4(x4) #8*8
+        print(f'x5: {x5.shape}')        
 
+        x_pool1 = self.pool1(x5)
+        x_pool2 = self.pool2(x5)
+        x_pool3 = self.pool3(x5)
+        x_pool4 = self.pool4(x5)
+        
+        enc_out = self.conv_pool(torch.cat((x_pool1, x_pool2, x_pool3, x_pool4), dim=1))
+        enc_out_up1 = F.upsample(enc_out, scale_factor=4, mode='bilinear', align_corners=True)
+        dec_low_level = self.conv_dec_low_level(x2)
+        enc_out_merge = self.merg_conv(torch.cat((enc_out_up1, dec_low_level), dim=1))
+        enc_out_final = F.upsample(enc_out_merge, scale_factor=4, mode='bilinear', align_corners=True)
+        crop_start = (enc_out_final.shape[-1]-101)//2
+        crop_end = crop_start + 101
+        x_out = enc_out_final[:,:,crop_start:crop_end,crop_start:crop_end].squeeze()
         #from boxx import g
         #g()
-        
-        x_up1 = self.up1(x5, x4)
-        x_up2 = self.up2(x_up1, x3)
-        x_up3 = self.up3(x_up2, x2)
-        x_up4 = self.up4(x_up3, x1)
-        x_out = self.outc(x_up4)
-        
-
-
         return x_out
+
+
+class Polling(nn.Module):
+    def __init__(self, in_ch, out_ch, upscale_size, dilation):
+        super(Polling, self).__init__()          
+
+        self.conv1 = nn.Conv2d(in_ch, out_ch, 1)
+        self.bn1 = nn.BatchNorm2d(out_ch)
+        #self.relu = nn.ReLU(inplace=True)
+        self.upscale = nn.Upsample(size=upscale_size, mode='bilinear', align_corners=True)																					
+        self.conv2 = conv_2d_depth_sep(out_ch, out_ch, kernel_size=3, dilation=dilation)
+        self.bn2 = nn.BatchNorm2d(out_ch)
+
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.upscale(out)
+        #from boxx import g
+        #g()
+        out = self.conv2(out)
+        #out = self.relu(out)
+        out = self.bn2(out)
+
+        return out
     
     
+    
+def resnet50(pretrained=False, **kwargs):
+    """Constructs a ResNet-50 model.
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+    """
+    model = ResNet(Bottleneck, [3, 4, 6, 3], **kwargs)
+    if pretrained:
+        model.load_state_dict(model_zoo.load_url(model_urls['resnet50']))
+    return model
