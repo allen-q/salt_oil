66455ea7dd451eab16985f7e59cfb36c8d387f7e
Allen Qin <allen.qin.au@gmail.com>
2018-09-29 01:18:31 +1000

    add bottleneck conv to reduce params

--- pytorch_unet/unet/resnet_v7.py
+++ pytorch_unet/unet/resnet_v7.py
@@ -51,7 +51,8 @@ class double_conv(nn.Module):
     def __init__(self, in_ch, out_ch):
         super(double_conv, self).__init__()
         self.conv = nn.Sequential(
-            nn.Conv2d(in_ch, out_ch, 3, padding=1),
+            nn.Conv2d(in_ch, in_ch//4, 1),
+            nn.Conv2d(in_ch//4, out_ch, 3, padding=1),
             nn.BatchNorm2d(out_ch),
             nn.ReLU(inplace=True),
             nn.Conv2d(out_ch, out_ch, 3, padding=1),
