139a4fce69fe6cc8477fa9c353b976af6119d935
Allen Qin <allen.qin.au@gmail.com>
2018-10-17 00:11:58 +1100

    reduced logit_image channel to 192 from 512

--- pytorch_unet/unet/resnet_deep_sup_v11.py
+++ pytorch_unet/unet/resnet_deep_sup_v11.py
@@ -439,12 +439,10 @@ class UResNet(nn.Module):
         self.decoder2 = Decoder(64+64, 64, 64)
         self.decoder1 = Decoder(64+64, 32, 64)
 
-        self.se_f = Seq_Ex_Block(832, 16)
-
-        self.outc = OutConv(320, logits=True)
+        self.se_f = Seq_Ex_Block(512, 16)
 
         self.logit_pixel  = nn.Sequential(
-            nn.Conv2d(832, 64, kernel_size=3, padding=1),
+            nn.Conv2d(512, 64, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
             nn.Conv2d( 64,  1, kernel_size=1, padding=0),
         )
@@ -456,10 +454,12 @@ class UResNet(nn.Module):
         )
 
         self.logit_image = nn.Sequential(
-            nn.Linear(512, 128),
+            nn.Linear(192, 64),
             nn.ReLU(inplace=True),
-            nn.Linear(128, 1),
+            nn.Linear(64, 1),
         )
+        
+        self.e5_bottleneck = nn.Conv2d(512, 192, kernel_size=1, padding=0)
 
     def crop_tensor(self, x, x_crop_start, x_crop_end):
         return x[:,:,x_crop_start:x_crop_end,x_crop_start:x_crop_end]
@@ -511,8 +511,8 @@ class UResNet(nn.Module):
                          self.crop_tensor(d1_logit, x_crop_start, x_crop_end)])
                  .squeeze().permute(1,0,2,3)
             )
-    
-        img_clf = F.adaptive_avg_pool2d(e5, output_size=1)
+        
+        img_clf = F.adaptive_avg_pool2d(self.e5_bottleneck(e5), output_size=1)
         f = torch.cat((
                 d1,
                 F.upsample(d2, scale_factor=2, mode='bilinear', align_corners=False),
@@ -520,7 +520,7 @@ class UResNet(nn.Module):
                 F.upsample(d4, scale_factor=8, mode='bilinear', align_corners=False),
                 F.upsample(d5, scale_factor=16, mode='bilinear', align_corners=False),
                 F.upsample(img_clf, scale_factor=128, mode='nearest')                
-                ), 1)               #320, 128, 128
+                ), 1)               #512, 128, 128
 
         f = self.se_f(f)
         f = F.dropout2d(f, p=0.5, training=self.training)
@@ -529,9 +529,5 @@ class UResNet(nn.Module):
         
         #img_clf = F.dropout(img_clf, p=0.50, training=self.training)
         logit_image = self.logit_image(img_clf.view(batch_size,-1)).view(-1)
-
-
-        #out = self.outc(f)          #1, 101,101
-
 
         return (logit_pixel, logit_image, logit_dec)
